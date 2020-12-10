f9161de191feabe41357a38a7a3c9721f2f68d2d
EmmaKengen <74139793+EmmaKengen@users.noreply.github.com>
2020-12-10 14:20:52 +0100

    Add files via upload

--- javascript/collusion.js
+++ javascript/collusion.js
@@ -1,3 +1,10 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
+
+/*hamburger menu*/
+/*https://codepen.io/shooft/pen/jOMOroZ*/
+var buttonNav=document.querySelector("header button:first-of-type");
+var contentNavigatie=document.querySelector("header nav");
+var website=document.querySelector("body");
+
